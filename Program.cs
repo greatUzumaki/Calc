566224a58a49105a88da9edd9af6881f1c57c4dc
Alexey Pargachev <pargachev20@gmail.com>
2020-06-22 23:57:10 +0700

    Add new strings , back in start

--- Program.cs
+++ Program.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Calc
+namespace ConsoleApp1
 {
     class Program
     {
@@ -14,9 +15,11 @@ namespace Calc
             double b;
             double otvet;
             char oper;
+            char next;
 
             Console.WriteLine("Math calc by uzumaki team!");
 
+        Start:
             Console.WriteLine("Введите первое число:");
             a = Convert.ToDouble(Console.ReadLine());
 
@@ -26,34 +29,59 @@ namespace Calc
             Console.WriteLine("Введите второе число:");
             b = Convert.ToDouble(Console.ReadLine());
 
-            if(oper == '+') //Sabuhi
+            if (oper == '+') //Sabuhi
             {
-                   otvet = a + b; 
-                 Console.WriteLine(otvet);
+                otvet = a + b;
+                Console.WriteLine("Ваш ответ = " + otvet);
+                Console.WriteLine("Продолжить? Д/Н");
+                next = Convert.ToChar(Console.ReadLine());
+                if (next == 'Д')
+                {
+                    goto Start;
+                }
             }
 
             if (oper == '-') //Anton
             {
                 Console.Write("Otvet: ");
-                Console.WriteLine(a-b);
+                Console.WriteLine("Ваш ответ = " +  (a - b));
+                Console.WriteLine("Продолжить? Д/Н");
+                next = Convert.ToChar(Console.ReadLine());
+                if (next == 'Д')
+                {
+                    goto Start;
+                }
             }
 
             if (oper == '*') //Sanya
             {
                 otvet = a * b;
-                Console.WriteLine(otvet);
+                Console.WriteLine("Ваш ответ = " + otvet);
+                Console.WriteLine("Продолжить? Д/Н");
+                next = Convert.ToChar(Console.ReadLine());
+                if (next == 'Д')
+                {
+                    goto Start;
+                }
             }
 
             if (oper == '/') //Lev or Leha
             {
-                if (b != 0) 
+                if (b != 0)
                 {
                     otvet = a / b;
-                    Console.WriteLine(otvet);
+                    Console.WriteLine("Ваш ответ = " + otvet);
+                    Console.WriteLine("Продолжить? Д/Н");
+                    next = Convert.ToChar(Console.ReadLine());
+                    if (next == 'Д')
+                    {
+                        goto Start;
+                    } 
+                    
                 }
             }
-            
-           
+
+      
             Console.ReadKey();
         }
     }
