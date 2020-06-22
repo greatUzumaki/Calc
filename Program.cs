392ec16ec39de27974d579f28dced8a489f915da
Alexey Pargachev <67081739+greatUzumaki@users.noreply.github.com>
2020-06-22 19:20:33 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double otvet;
+            char oper;
+
+            Console.WriteLine("Math calc by uzumaki team!");
+
+            Console.WriteLine("Введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор:");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if(oper == '+') //Sabuhi
+            {
+
+            }
+
+            if (oper == '-') //Anton
+            {
+
+            }
+
+            if (oper == '*') //Sanya
+            {
+
+            }
+
+            if (oper == '/') //Lev or Leha
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
